7cb7e958e988f7dfa498fc9061aa59a18d4f4d3f
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-10 22:50:16 +0200

    add twitter login

--- server.js
+++ server.js
@@ -8,14 +8,46 @@ var url = 'mongodb://localhost:27017/pinterest_clone';
 var session = require('express-session');
 var path = require('path');
 
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+/*var oauth = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    "AHI1ElA7WMFzF4QhzZcxlMdVP",
+    "EcxtYxF8ochjraTnvrLpGYujQHMpERDLHcr4bipB9WVwrq8e5h",
+    "1.0",
+    "https://pinterest-cln.herokuapp.com/auth/twitter/callback",
+    "HMAC-SHA1"
+);*/
+
 app.use(express.static('public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(session({secret: "secretword", resave: false, saveUninitialized: true}));
 
 app.get('/', function (req, res) {
-    res.render('layout.jade', {});
+    res.render('layout.jade', {username: req.user.username});
 });
+
+passport.use(new TwitterStrategy({
+        consumerKey: 'AHI1ElA7WMFzF4QhzZcxlMdVP',
+        consumerSecret: 'EcxtYxF8ochjraTnvrLpGYujQHMpERDLHcr4bipB9WVwrq8e5h',
+        callbackURL: "https://pinterest-cln.herokuapp.com/auth/twitter/callback"
+    },
+    function (token, tokenSecret, profile, done) {
+        User.findOrCreate({twitterId: profile.id}, function (error, user) {
+            return done(error, user);
+        })
+    }
+));
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { successRedirect: '/',
+        failureRedirect: '/login' }));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Listening port 3000');
