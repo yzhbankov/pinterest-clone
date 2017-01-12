d46445f4d0535ea164e86ddccf9cf96cb3e53ccc
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-12 23:27:19 +0200

    add picture additing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
 var url = 'mongodb://localhost:27017/pinterest_clone';
 var session = require('express-session');
 var passport = require('passport');
@@ -32,6 +33,7 @@ passport.use(new TwitterStrategy({
 app.use(express.static('public'));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(session({secret: "secretword", resave: false, saveUninitialized: true}));
@@ -39,9 +41,30 @@ app.use(session({secret: "secretword", resave: false, saveUninitialized: true}))
 app.get('/', function (req, res) {
     if (req.session.passport) {
         var username = req.session.passport.user.username;
-        res.render('layout.jade', {"username": username});
+        var profile_img = req.session.passport.user._json.profile_image_url;
+
+        MongoClient.connect(url, function (err, db) {
+            db.collection('users').findOne({"username": username}, function (err, item) {
+                if (item) {
+                    db.close();
+                    console.log("user already exist");
+                } else {
+                    db.collection('users').insertOne({
+                        "username": username,
+                        "profile_img": profile_img
+                    }, function (err, result) {
+                        if (!err) {
+                            console.log("user added successfuly");
+                        }
+                    });
+                    db.close();
+                }
+            });
+        });
+
+        res.render('index.jade', {"username": username});
     } else {
-        res.render('layout.jade', {});
+        res.render('index.jade', {});
     }
 });
 
@@ -55,9 +78,73 @@ app.get('/auth/twitter/callback',
         failureRedirect: '/error'
     }));
 
-app.get('/error',
-    function () {
-        console.log('Something is wrong');
+app.post('/add', function (req, res) {
+    var pic_url = req.body.pic_url;
+    var description = req.body.description;
+    var username = req.session.passport.user.username;
+    var profile_img = req.session.passport.user._json.profile_image_url;
+    console.log(username);
+    MongoClient.connect(url, function (err, db) {
+        db.collection('pictures').findOne({"username": username, "pic_url": pic_url}, function (err, item) {
+            if (item) {
+                db.close();
+                console.log("picture already exist");
+            } else {
+                db.collection('pictures').insertOne({
+                    "username": username,
+                    "pic_url": pic_url,
+                    "description": description,
+                    "profile_img": profile_img
+                }, function (err, result) {
+                    if (!err) {
+                        console.log("picture added successfuly");
+                    }
+                });
+                db.close();
+            }
+        });
+    });
+    res.redirect('/');
+});
+
+app.get('/allpics', function (req, res) {
+    if (!req.session.passport) {
+        console.log("user is not authorized");
+        res.redirect('/');
+    } else {
+        MongoClient.connect(url, function (err, db) {
+            var resent = db.collection('pictures').find({}, {
+                'username': true,
+                "pic_url": true,
+                'description': true,
+                "profile_img":true
+            }).toArray(function (err, result) {
+                if (result.length < 1) {
+                    console.log('no pictures found');
+                    res.render('allpics.jade', {"pics": []});
+                } else {
+                    console.log('pictures found');
+                    var users = [];
+                    var pics_url = [];
+                    var descriptions = [];
+                    var profiles = [];
+                    for (var i = 0; i < result.length; i++) {
+                        users.push(result[i].username);
+                        pics_url.push(result[i].pic_url);
+                        descriptions.push(result[i].description);
+                        profiles.push(result[i].profile_img);
+                    }
+                    res.render('allpics.jade', {
+                        "users": users,
+                        "pics_url": pics_url,
+                        "descriptions": descriptions,
+                        "profiles": profiles
+                    });
+                }
+            });
+            db.close();
+        });
+    }
     });
 
 app.get('/logout', function (req, res) {
