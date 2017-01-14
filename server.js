f873112e9eb55b40185570206850b7dd14d5065d
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-14 21:30:36 +0200

    login/logout visible when user is authorised

--- server.js
+++ server.js
@@ -42,32 +42,59 @@ app.set('view engine', 'jade');
 app.use(session({secret: "secretword", resave: false, saveUninitialized: true}));
 
 app.get('/', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        var resent = db.collection('pictures').find({}, {
+            'username': true,
+            "pic_url": true,
+            'description': true,
+            "profile_img": true
+        }).toArray(function (err, result) {
+            if (result.length < 1) {
+                console.log('no pictures found');
+                res.render('allpics.jade', {"profile_img": profile_img, "pics_url": []});
+            } else {
+                var users = [];
+                var pics_url = [];
+                var descriptions = [];
+                var profiles = [];
+                for (var i = 0; i < result.length; i++) {
+                    users.push(result[i].username);
+                    pics_url.push(result[i].pic_url);
+                    descriptions.push(result[i].description);
+                    profiles.push(result[i].profile_img);
+                }
+                res.render('allpics.jade', {
+                    "profile_img": profile_img,
+                    "username": username,
+                    "users": users,
+                    "pics_url": pics_url,
+                    "descriptions": descriptions,
+                    "profiles": profiles
+                });
+            }
+        });
+        db.close();
+    });
+
     if (req.session.passport) {
         var username = req.session.passport.user.username;
         var profile_img = req.session.passport.user._json.profile_image_url;
-        console.log(profile_img);
         MongoClient.connect(url, function (err, db) {
             db.collection('users').findOne({"username": username}, function (err, item) {
                 if (item) {
                     db.close();
-                    console.log("user already exist");
                 } else {
                     db.collection('users').insertOne({
                         "username": username,
                         "profile_img": profile_img
                     }, function (err, result) {
                         if (!err) {
-                            console.log("user added successfuly");
                         }
                     });
                     db.close();
                 }
             });
         });
-
-        res.render('index.jade', {"username": username, "profile_img": profile_img});
-    } else {
-        res.render('index.jade', {});
     }
 });
 
@@ -127,7 +154,7 @@ app.get('/allpics', function (req, res) {
             }).toArray(function (err, result) {
                 if (result.length < 1) {
                     console.log('no pictures found');
-                    res.render('allpics.jade', {"profile_img":profile_img,"pics_url": []});
+                    res.render('allpics.jade', {"profile_img": profile_img, "pics_url": []});
                 } else {
                     console.log('pictures found');
                     var users = [];
@@ -141,7 +168,7 @@ app.get('/allpics', function (req, res) {
                         profiles.push(result[i].profile_img);
                     }
                     res.render('allpics.jade', {
-                        "profile_img":profile_img,
+                        "profile_img": profile_img,
                         "username": username,
                         "users": users,
                         "pics_url": pics_url,
@@ -171,7 +198,7 @@ app.get('/mypics', function (req, res) {
                 "profile_img": true
             }).toArray(function (err, result) {
                 if (result.length < 1) {
-                    res.render('mypics.jade', {"profile_img":profile_img,"pics_url": []});
+                    res.render('mypics.jade', {"profile_img": profile_img, "pics_url": []});
                 } else {
                     var users = [];
                     var id = [];
@@ -209,7 +236,7 @@ app.get('/delete/:id', function (req, res) {
         var username = req.params.username;
         var id = req.params.id;
         MongoClient.connect(url, function (err, db) {
-            db.collection('pictures').remove({"id":id});
+            db.collection('pictures').remove({"id": id});
             db.close();
             res.redirect('/mypics');
 
